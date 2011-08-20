fad77ae2ab89a4b563b20eb2527da653c4934589
Jason Staten <jstaten07@gmail.com>
2011-08-20 15:37:32 -0600

    Works in-memory

--- script/app.js
+++ script/app.js
@@ -1,11 +1,48 @@
 (function(){
+
+  function TodoItem(list, value, index) {
+    this.list = list;
+    this.value = ko.observable(value);
+    this.complete = ko.observable(false);
+    this.mode = ko.observable("view");
+  }
+  TodoItem.prototype.destroy = function() {
+    this.list.removeItem(this);
+  };
+
+  TodoItem.prototype.edit = function() {
+    this.mode("edit");
+  };
+
+  TodoItem.prototype.save = function() {
+    this.mode("view");
+  };
+
   var todosModel = {
     items: ko.observableArray(),
     currentText: ko.observable(""),
     addItem: function() {
-      this.items.push(this.currentText());
+      var item = new TodoItem(this, this.currentText());
+      this.items.push(item);
       this.currentText("");
-      console.dir(this.items());
+    },
+    removeItem: function(item) {
+      this.items.remove(item);
+    },
+    render: function(item) {
+      return item.mode() + "Item";
+    },
+    remaining: function() {
+      return _.select(this.items(), function(i){return !i.complete()});
+    },
+    completed: function() {
+      return _.select(this.items(), function(i){return i.complete()});
+    },
+    clearCompleted: function() {
+      _.each(this.completed(), function(i){i.destroy()});
+    },
+    pluralize: function(length) {
+      return length == 1 ? 'item' : 'items';
     }
   };
 
